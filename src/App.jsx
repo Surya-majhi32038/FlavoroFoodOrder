93d2b6b0a18a6433c6c3ba4e779e1b22cd8a3f5e
Surya-majhi32038 <suryamajhi32038@gmail.com>
2024-10-10 18:14:03 +0530

    change home

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter,Route,Routes } from 'react-router-dom'
-import Home from './pages/home'
+import Home from './pages/Home'
 import Success from './pages/Success'
 import Error from './pages/Error'
 
